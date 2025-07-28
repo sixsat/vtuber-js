f8a067360f5e1a27d446cd2a2a334aae633704f7
sat <77387934+sixsat@users.noreply.github.com>
2025-07-28 20:03:21 +0700

    fix init avatar

--- src/components/AvatarViewer.jsx
+++ src/components/AvatarViewer.jsx
@@ -6,12 +6,16 @@ import { PerspectiveCamera } from '@react-three/drei';
 export default function AvatarViewer() {
   return (
     <Canvas style={{ height: '100vh', width: '100%' }}>
-      <PerspectiveCamera makeDefault position={[0, 1.6, 1.2]} fov={10} />
+      <PerspectiveCamera makeDefault position={[0, 1.5, 2]} fov={10} />
 
       <ambientLight intensity={0.7} />
       <directionalLight position={[0, 1, 1]} intensity={1.5} />
-      <OrbitControls enableRotate={false} enableZoom={false} enablePan={false} />
-
+      <OrbitControls
+        enableRotate={false}
+        enableZoom={false}
+        enablePan={false}
+        target={[0, 1.39, 0]}
+      />
       <AvatarModel />
     </Canvas>
   );
