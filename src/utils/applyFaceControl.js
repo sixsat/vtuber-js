cab2a652c0f7fc1c010b9079f1511229558a4d8f
sat <77387934+sixsat@users.noreply.github.com>
2025-07-28 15:42:50 +0700

    update mouth

--- src/utils/applyFaceControl.js
+++ src/utils/applyFaceControl.js
@@ -1,4 +1,8 @@
 export function applyFaceControlsToMesh(vrm, controls) {
+  if (!vrm || !controls) {
+    return;
+  }
+
   const { mouthOpen, eyeLeftClose, eyeRightClose } = controls;
 
   // Apply to blend shape by mesh
@@ -7,25 +11,17 @@ export function applyFaceControlsToMesh(vrm, controls) {
       const dict = obj.morphTargetDictionary;
       const influences = obj.morphTargetInfluences;
 
+      // Eyes
       if ('Fcl_EYE_Close_L' in dict) {
         influences[dict['Fcl_EYE_Close_L']] = eyeLeftClose;
       }
-
       if ('Fcl_EYE_Close_R' in dict) {
         influences[dict['Fcl_EYE_Close_R']] = eyeRightClose;
       }
 
+      // Mouth
       if ('Fcl_MTH_A' in dict) {
         influences[dict['Fcl_MTH_A']] = mouthOpen;
-      }
-      if ('Fcl_MTH_Large' in dict) {
-        influences[dict['Fcl_MTH_Large']] = mouthOpen;
-      }
-      if ('Fcl_MTH_Down' in dict) {
-        influences[dict['Fcl_MTH_Down']] = mouthOpen * 0.5;
-      }
-      if ('Fcl_MTH_Up' in dict) {
-        influences[dict['Fcl_MTH_Up']] = mouthOpen * 0.5;
       }
     }
   });
